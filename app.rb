81b70a7b50da6b4ab46f06ea7a661da844bb310e
Oldboy <vengoshara@gmail.com>
2019-02-11 23:56:24 +0300

    define get_db

--- app.rb
+++ app.rb
@@ -4,15 +4,15 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 configure do
-	@db = SQLite3::Database.new "barbershop.db"
-	@db.execute 'CREATE TABLE IF NOT EXISTS
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
 		"Users"
 		(
 			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
-			"username" TEXT
-			"phone" TEXT
-			"datestampe" TEXT
-			"barber" TEXT
+			"username" TEXT,
+			"phone" TEXT,
+			"datestamp" TEXT,
+			"barber" TEXT,
 			"color" TEXT
 		)'
 end
@@ -48,6 +48,21 @@ post '/visit' do
 		return erb :visit
 	end
 
+	db = get_db
+	db.execute 'insert into 
+		Users (
+			username,
+			phone,
+			datestamp,
+			barber,
+			color
+			)
+			values(?,?,?,?,?)', [@username, @phone, @datetime, @barber, @color]
+
 	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
 
+end
+
+def get_db
+	return SQLite3::Database.new 'barbershop.db'
 end
